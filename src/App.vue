032b941546b91ee802d94eb62cd149cb6cda8e76
Ulrich Feindt <ufeindt@gmail.com>
2025-02-08 12:04:23 +0100

    Add header and empty div for table

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PeriodicTable from './components/PeriodicTable.vue'
+</script>
 
 <template>
-  <header></header>
+  <header>
+    <h1>The Periodic Table of Elements</h1>
+  </header>
 
-  <main></main>
+  <main>
+    <PeriodicTable />
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  margin-bottom: 20px;
+  margin-top: 20px;
+  width: 100%;
+}
+
+h1 {
+  text-align: center;
+  width: 100%;
+}
+</style>
---
+++ src/components/PeriodicTable.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="table"></div>
+</template>
+
+<style scoped>
+.table {
+  background-color: var(--color-background-mute);
+  border-radius: 10px;
+  margin: 0px auto;
+  min-height: 100px;
+  width: 1000px;
+}
+</style>
